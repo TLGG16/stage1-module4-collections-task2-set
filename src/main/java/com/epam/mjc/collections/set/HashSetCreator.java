b7855359bd5d3d4644801866ee4c4095be665540
TLGG16 <TLGG16@yandex.ru>
2023-06-18 20:55:28 +0300

    kekw

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -5,5 +5,21 @@ import java.util.List;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        HashSet<Integer> answ = new HashSet<>();
+        for (Integer i: sourceList
+             ) {
+            if (i%2==0){
+                int buf = i;
+                answ.add(buf);
+                do {
+                    buf/=2;
+                    answ.add(buf);
+                } while (buf%2==0);
+            }else {
+                answ.add(i);
+                answ.add(i*2);
+            }
+        }
+        return answ;
     }
 }
--- src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
@@ -2,8 +2,23 @@ package com.epam.mjc.collections.set;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SetCombinationCreator {
     public Set<String> createSetCombination(Set<String> firstSet, Set<String> secondSet, Set<String> thirdSet) {
+        Set<String> answ = new TreeSet<>();
+        for (String str:firstSet
+             ) {
+            if (secondSet.contains(str)&&!thirdSet.contains(str)){
+                answ.add(str);
+            }
+        }
+        for (String str: thirdSet
+             ) {
+            if (!firstSet.contains(str)&& !secondSet.contains(str)){
+                answ.add(str);
+            }
+        }
+        return answ;
     }
 }
